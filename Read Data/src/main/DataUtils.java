381d2b87ef031d3ec0c70539fdb6e5de604cee80
Harman Singh <harman.singh@hotmail.co.nz>
2016-09-25 18:31:36 +1300

    add read data utility class

---
+++ Read Data/src/main/DataUtils.java
@@ -0,0 +1,79 @@
+package main;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DataUtils {
+	public static final int classLabels = 9; //start from 0
+
+	public static void toCSV(final String filename) {
+		String outputFilename = "csv_" + filename;
+		System.out.println("Reading file: " + filename);
+		try (Scanner din = new Scanner(new File(filename)); FileWriter fw = new FileWriter(outputFilename, true);) {
+			while (din.hasNextLine()) {
+				// if line contains any of the three class labels
+				// then replace it with its replacement binary digits
+				//
+				Scanner line = new Scanner(din.nextLine());
+				while (line.hasNextInt()) {
+					Integer value = line.nextInt();
+					fw.append(value.toString());
+					if (line.hasNextInt()) {
+						fw.append(",");
+					}
+				}
+				line.close();
+				fw.append("\n");
+
+			}
+			System.out.println("File converted to cvs: " + outputFilename);
+		} catch (IOException e) {
+			throw new RuntimeException("Data File caused IO exception");
+		}
+	}
+	
+	public static void toBPNNFormat(final String filename) {
+		String outputFilename = "bpnn_" + filename;
+		System.out.println("Reading file: " + filename);
+		try (Scanner din = new Scanner(new File(filename)); FileWriter fw = new FileWriter(outputFilename, true);) {
+			while (din.hasNextLine()) {
+				// if line contains any of the three class labels
+				// then replace it with its replacement binary digits
+				//
+				Scanner line = new Scanner(din.nextLine());
+				while (line.hasNextInt()) {
+					Integer value = line.nextInt();
+					if (line.hasNextInt()) {
+						fw.append(value.toString());
+						fw.append(" ");
+					} else{
+						// read integer 
+						// convert to neural network output
+						// if 10 then class output = 0 0 0 0 1 0 0 0 0 0 = 6th class label
+						
+						for(int i = classLabels; i > value; i--){
+							fw.append("0 ");
+						}
+						fw.append("1");
+						for(int i = value; i > 0; i--){
+							fw.append(" 0");
+						}
+					}
+				}
+				line.close();
+				fw.append("\n");
+
+			}
+			System.out.println("File converted to bpnn formmat: " + outputFilename);
+		} catch (IOException e) {
+			throw new RuntimeException("Data File caused IO exception");
+		}
+	}
+	
+	public static void main(String[] args) {
+		String filename = "digits60";
+		toBPNNFormat(filename);
+	}
+}
